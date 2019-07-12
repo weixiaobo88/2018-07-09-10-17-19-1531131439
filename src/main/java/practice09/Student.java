80d2bb5fa823ac42dff2d49f2d0dbfd780d38201
Zhaoyu <weixiaobo88@gmail.com>
2019-07-12 12:54:37 +0800

    finish practice09

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,24 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        String klassName = this.klass.getDisplayName();
+        String introduction = super.introduce();
+        if (this.equals(this.klass.getLeader())) {
+            return introduction + " I am a Student. I am Leader of " + klassName + ".";
+        }
+        return introduction + " I am a Student. I am at " + klassName + ".";
+    }
 }
