4b60acd83353998d3fe2ce7548b08e05628b1ef8
Zhaoyu <weixiaobo88@gmail.com>
2019-07-12 17:17:04 +0800

    finish practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,58 @@
 package practice11;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private ArrayList<Student> students = new ArrayList<>();
+    private LinkedList<Teacher> teachers = new LinkedList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
+
+    public void assignLeader(Student student) {
+        if (this.students.contains(student)) {
+            this.leader = student;
+            this.notifyTeacherOnAssignLeader(student, this);
+        } else {
+            System.out.println("It is not one of us.");
+        }
+    }
+
+    private void notifyTeacherOnAssignLeader(Student student, Klass klass) {
+        for (Teacher teacher : teachers) {
+            teacher.receiveNotifyOnAssignLeader(student, klass);
+        }
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void appendMember(Student student) {
+        this.students.add(student);
+        this.notifyTeachersOnAppendMember(student, this);
+    }
+
+    private void notifyTeachersOnAppendMember(Student student, Klass klass) {
+        for (Teacher teacher : teachers) {
+            teacher.receiveNotifyOnAppendMember(student, klass);
+        }
+    }
+
+    void attachTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,67 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        for(Klass klass : classes) { klass.attachTeacher(this);}
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+        String introduction = super.introduce();
+
+        if (null == this.classes) {
+            return introduction +  " I am a Teacher. I teach No Class.";
+        }
+
+        return introduction +  " I am a Teacher. I teach Class " + this.getFormattedClasses() + ".";
+    }
+
+    private String getFormattedClasses() {
+        String result = "";
+        for (Klass klass : classes) {
+            if (classes.indexOf(klass) == classes.size() -1 ) {
+                result += klass.getNumber();
+            } else {
+                result += klass.getNumber() + ", ";
+            }
+        }
+        return result;
+    }
+
+    public String introduceWith(Student student) {
+        String introduction = super.introduce();
+        if (isTeaching(student)) {
+            return introduction +  " I am a Teacher. I teach " + student.getName() + ".";
+        }
+        return introduction +  " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return this.classes;
+    }
+
+    public boolean isTeaching(Student student) {
+        Klass studentKlass = student.getKlass();
+
+        return classes.contains(studentKlass);
+    }
+
+    void receiveNotifyOnAppendMember(Student student, Klass klass) {
+        System.out.println("I am " + this.getName() + ". I know " + student.getName() + " has joined " + klass.getDisplayName() + ".");
+    }
+
+    void receiveNotifyOnAssignLeader(Student student, Klass klass) {
+        System.out.println("I am " + this.getName() + ". I know " + student.getName() + " become Leader of " + klass.getDisplayName() + ".");
+    }
 }
+
