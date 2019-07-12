a7f0b4b0215e3ffdf0f883a7c0172d4a6fad99ef
Zhaoyu <weixiaobo88@gmail.com>
2019-07-12 11:56:51 +0800

    finish practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,17 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public String getDisplayName() {
+        return "Class 2";
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,36 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        String introduction = super.introduce();
+
+        if (this.klass == null) {
+            return introduction + " I am a Teacher. I teach No Class.";
+        }
+        return introduction + " I am a Teacher. I teach " + this.klass.getDisplayName() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        String introduction = super.introduce();
+        if (this.klass.getNumber() == student.getKlass().getNumber()) {
+            return introduction + " I am a Teacher. I teach " +  student.getName() + ".";
+        }
+        return introduction + " I am a Teacher. I don't teach " +  student.getName() + ".";
+    }
 }
